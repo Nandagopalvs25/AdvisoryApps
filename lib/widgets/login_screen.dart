16f468bd4268e165d025fe6eedaf49d5d03d35ec
Abhijith.U <79012764+abhijithu1@users.noreply.github.com>
2023-06-28 19:20:34 +0530

    login screen changes

--- lib/widgets/login_screen.dart
+++ lib/widgets/login_screen.dart
@@ -12,7 +12,6 @@ import '../routes.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-
 class login extends StatefulWidget {
   const login({super.key});
 
@@ -41,30 +40,34 @@ class _loginState extends State<login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.green.shade100,
+      backgroundColor: const Color.fromARGB(255, 255, 255, 255),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Container(
-              height: 200,
-              child: Center(
-                child: Image.network(
-                    'https://ist7-1.filesor.com/pimpandhost.com/2/9/8/7/298728/f/t/u/x/ftuxN/Screenshot__42_-removebg-preview.png'),
-              )),
-          TextField(
-            controller: _username,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(100),
-                  borderSide: const BorderSide(style: BorderStyle.solid)),
-              labelText: 'Username',
-              icon: const FaIcon(FontAwesomeIcons.user),
-              floatingLabelAlignment: FloatingLabelAlignment.center,
-              iconColor: const Color.fromARGB(164, 23, 134, 135),
-              focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(20),
-                borderSide: const BorderSide(
-                  color: Color.fromARGB(255, 141, 39, 243),
+            height: 200,
+            child: Center(
+              child: Image.network(
+                  'https://ist7-1.filesor.com/pimpandhost.com/2/9/8/7/298728/f/t/u/x/ftuxN/Screenshot__42_-removebg-preview.png'),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 30, right: 30),
+            child: TextField(
+              controller: _username,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(100),
+                    borderSide: const BorderSide(style: BorderStyle.solid)),
+                labelText: 'Username',
+                icon: const FaIcon(FontAwesomeIcons.user),
+                floatingLabelAlignment: FloatingLabelAlignment.center,
+                iconColor: const Color.fromARGB(164, 23, 134, 135),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  borderSide: const BorderSide(
+                    color: Color.fromARGB(255, 141, 39, 243),
+                  ),
                 ),
               ),
             ),
@@ -72,21 +75,24 @@ class _loginState extends State<login> {
           const SizedBox(
             height: 10,
           ),
-          TextField(
-            controller: _password,
-            obscureText: true,
-            decoration: InputDecoration(
-              iconColor: const Color.fromARGB(164, 23, 134, 135),
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(30),
-              ),
-              labelText: 'Password',
-              icon: const FaIcon(FontAwesomeIcons.key),
-              floatingLabelAlignment: FloatingLabelAlignment.center,
-              focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(20),
-                borderSide: const BorderSide(
-                  color: Color.fromARGB(255, 141, 39, 243),
+          Padding(
+            padding: const EdgeInsets.only(left: 30, right: 30),
+            child: TextField(
+              controller: _password,
+              obscureText: true,
+              decoration: InputDecoration(
+                iconColor: const Color.fromARGB(164, 23, 134, 135),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                labelText: 'Password',
+                icon: const FaIcon(FontAwesomeIcons.key),
+                floatingLabelAlignment: FloatingLabelAlignment.center,
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  borderSide: const BorderSide(
+                    color: Color.fromARGB(255, 141, 39, 243),
+                  ),
                 ),
               ),
             ),
@@ -105,7 +111,8 @@ class _loginState extends State<login> {
                 if (res.statusCode == 200) {
                   Map<String, dynamic> user = jsonDecode(res.body);
                   String token = user['key'];
-                  SharedPreferences prefs = await SharedPreferences.getInstance();
+                  SharedPreferences prefs =
+                      await SharedPreferences.getInstance();
                   await prefs.setString("username", (username));
                   await prefs.setString("token", token);
                   Navigator.of(context)?.pushNamedAndRemoveUntil(
@@ -113,20 +120,25 @@ class _loginState extends State<login> {
                 }
               },
               style: ElevatedButton.styleFrom(
-                backgroundColor: const Color.fromARGB(144, 131, 255, 216),
+                backgroundColor: Color.fromARGB(225, 255, 187, 40),
                 shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30),
-                  side: const BorderSide(width: 3,color: Color.fromARGB(255, 155, 36, 215))
-                ),
+                    borderRadius: BorderRadius.circular(30),
+                    side: const BorderSide(
+                        width: 3, color: Color.fromARGB(255, 245, 224, 255))),
               ),
-              child:  Row(
+              child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   const FaIcon(FontAwesomeIcons.rightToBracket),
                   const SizedBox(
                     width: 15,
                   ),
-                  Text("Log in",style: GoogleFonts.firaSans(fontSize: 20,color: const Color.fromARGB(211, 161, 0, 137)),)
+                  Text(
+                    "Log in",
+                    style: GoogleFonts.firaSans(
+                        fontSize: 20,
+                        color: const Color.fromARGB(211, 161, 0, 137)),
+                  )
                 ],
               ),
             ),
