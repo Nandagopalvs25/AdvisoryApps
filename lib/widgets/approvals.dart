d080737aa200fa620045b434e12ac569713c7f1a
Abhijith.U <79012764+abhijithu1@users.noreply.github.com>
2023-06-28 15:55:15 +0530

    Did some changes on login screen

--- lib/widgets/approvals.dart
+++ lib/widgets/approvals.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 
 class Approvals extends StatelessWidget {
   const Approvals({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Text("APPROVALS SECTION"),
     );
   }
